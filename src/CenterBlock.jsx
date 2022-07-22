87f682ba5d9df36edcca1b1311a2f6fa03ecbc86
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-07-22 11:19:16 +0300

    start Central block

--- src/CenterBlock.jsx
+++ src/CenterBlock.jsx
@@ -1,6 +1,15 @@
+import Search from './Search';
+import Filter from './Filter';
+import Content from './Content';
+
 function CenterBlock() {
     return (
-        <p>CenterBlock</p>
+        <div className="main__centerblock centerblock">
+            <Search />
+            <h2 className="centerblock__h2">Треки</h2>
+            <Filter />
+            <Content />
+        </div>
     );
 }
 
