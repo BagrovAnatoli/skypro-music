08b20e01c1d7e410b9e039f0d1e9e8fa012729e2
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-07-22 11:29:03 +0300

    add Search

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,7 +1,10 @@
 function Search() {
     return (
-        <div>
-            Search
+        <div className="centerblock__search search">
+            <svg className="search__svg">
+                <use xlinkHref="img/icon/sprite.svg#icon-search"/>
+            </svg>
+            <input className="search__text" type="search" placeholder="Поиск" name="search" />
         </div>
     );
 }
