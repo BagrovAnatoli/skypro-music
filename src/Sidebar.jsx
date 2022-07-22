a2e940efec623abdd7da8b5e02943cad5684bdda
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-07-22 10:58:58 +0300

    fill Sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,14 +1,48 @@
 import Personal from './Personal';
 
 function Sidebar() {
+    const sidebarDatas = [
+        {
+            href: "",
+            imgSrc: "img/playlist01.png",
+            imgAlt: "day's playlist",
+        },
+        {
+            href: "",
+            imgSrc: "img/playlist02.png",
+            imgAlt: "day's playlist",
+        },
+        {
+            href: "",
+            imgSrc: "img/playlist03.png",
+            imgAlt: "day's playlist",
+        },
+    ]
+
+    const sidebarItems = [];
+
+    for (let i = 0; i < sidebarDatas.length; i += 1) {
+        sidebarItems.push(
+            <div className="sidebar__item">
+                <a href={sidebarDatas[i].href} className="sidebar__link">
+                    <img 
+                        src={sidebarDatas[i].imgSrc} 
+                        alt={sidebarDatas[i].imgAlt} 
+                        className="sidebar__img" 
+                    />
+                </a>
+            </div>
+        );
+    }
+
     return (
         <div className="main__sidebar sidebar">
             <Personal name="Sergey.Ivanov"/>
-            {/* <div className="sidebar__block">
+            <div className="sidebar__block">
                 <div className="sidebar__list">
                     {sidebarItems}
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
