bbd68e7fd2f83df693647e1936799db21d3918e0
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-07-27 19:01:01 +0300

    map instead for in Burger.jsx

--- src/Burger.jsx
+++ src/Burger.jsx
@@ -1,9 +1,7 @@
 function Burger() {
 
-    const lines = [];
-    for (let i = 0; i < 3; i += 1) {
-        lines.push(<span className="burger__line" key={i}/>);
-    }
+    // eslint-disable-next-line react/no-array-index-key
+    const lines = Array.from({lenght: 3}).map((element, index) => <span className="burger__line" key={index}/>);
 
     return (
         <div className="nav__burger burger">
