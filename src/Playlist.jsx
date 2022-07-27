e65a85e6b4f4410ab9a82627c00615f1790da0b3
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-07-27 19:17:51 +0300

    map instead for in Playlist.jsx, add id key to tracksInfo.json

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -1,13 +1,12 @@
-// import DivSvgUse from './DivSvgUse';
 import PlaylistItem from './PlaylistItem';
 import tracks from './tracksInfo.json';
 
 function Playlist () {
-    const list = [];
-    for (let i = 0; i < tracks.length; i += 1) {
-        const item = <PlaylistItem data={tracks[i]} key={i}/>
-        list.push(item);
-    }
+
+    const list = tracks.map(track => (
+        <PlaylistItem data={track} key={track.id}/>
+    ));
+
     return (
         <div className="content__playlist playlist">
             {list}
