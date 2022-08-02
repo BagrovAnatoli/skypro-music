16abd49cdcc4f9a605fd496736b5fb19c10e3482
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-08-02 14:43:42 +0300

    prepare filter popup logic

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,6 +1,6 @@
-function Filter() {
+function Filter({handleFilterClick}) {
     return (
-        <div className="centerblock__filter filter">
+        <div className="centerblock__filter filter" onClick={handleFilterClick} aria-hidden="true">
             <div className="filter__title">Искать по:</div>
             <div className="filter__button button-author _btn-text">исполнителю</div>
             <div className="filter__button button-year _btn-text">году выпуска</div>
