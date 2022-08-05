8f7e719eaad8bc808fc2295c5b121d18c51c66cf
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-08-05 19:27:23 +0300

    add load emulation

--- src/Contain.jsx
+++ src/Contain.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import DivSvgUse from './DivSvgUse';
 
 function Contain() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 5000);
+
+    const isLoadingContain =  (
+        <div className="track-play__contain">
+            <div className="track-play__image" />
+            <div className="track-play__author track-play__author_loading"/>
+            <div className="track-play__album track-play__album_loading"/>
+        </div>
+    );
+
     const imageProps = {
         divProps: {
             className: "track-play__image",
@@ -13,6 +29,10 @@ function Contain() {
             xlinkHref: "img/icon/sprite.svg#icon-note",
         },
     };
+    if (isLoading) {
+        return isLoadingContain;
+    }
+
     return (
         <div className="track-play__contain">
             <DivSvgUse attrs={imageProps}/>
--- src/PlaylistItem.jsx
+++ src/PlaylistItem.jsx
@@ -1,17 +1,40 @@
+import { useState } from 'react';
 import TrackTitle from "./TrackTitle";
 import TrackAuthor from "./TrackAuthor";
 import TrackAlbum from "./TrackAlbum";
 import TrackTime from "./TrackTime";
 
 function PlaylistItem ( {data} ) {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 5000);
+
+    const loadingTrack = (
+        <div className="playlist__track track">
+            <div className="track__title">
+                <div className="track__title-image track__title-image_loading"/>
+                <div className="track__title-text track__title-text_loading"/>
+            </div>
+            <div className="track__author track__author_loading"/>
+            <div className="track__album track__album_loading"/>
+        </div>
+    );
+
+    const loadedTrack = (
+        <div className="playlist__track track">
+            <TrackTitle data={data.title}/>
+            <TrackAuthor data={data.author}/>
+            <TrackAlbum data={data.album}/>
+            <TrackTime data={data.time}/>
+        </div>
+    );
+
     return (
         <div className="playlist__item">
-            <div className="playlist__track track">
-                <TrackTitle data={data.title}/>
-                <TrackAuthor data={data.author}/>
-                <TrackAlbum data={data.album}/>
-                <TrackTime data={data.time}/>
-            </div>
+            {isLoading ? loadingTrack : loadedTrack}
         </div>
     );
 }
